ab20dda84afc743d1bd1bfc22ed6e0090af5b703
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-01-28 17:19:49 -0400

    (feat) Add Product component and render on Layout

---
+++ client/src/components/Product.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface Props {
+    
+}
+ 
+const Product : React.FunctionComponent<Props> = () => {
+    return (<React.Fragment>
+        Hi!
+    </React.Fragment>);
+}
+ 
+export default Product ;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from '@material-ui/core/styles';
 
 import Layout from '../components/Layout';
+import Product from '../components/Product';
 import customTheme from '../config/theme';
 
 const IndexPage = () => (
   <ThemeProvider theme={customTheme}>
     <Layout title='RC Store'>
       <h1>Hello Next.js 👋</h1>
+      <Product />
     </Layout>
   </ThemeProvider>
 );
