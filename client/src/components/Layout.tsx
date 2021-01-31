e5c6b62c0a0fbe0af753384a3ed2c059a9e96d29
Phuoc Tran <phuoctran27@gmail.com>
2021-01-30 19:51:55 -0800

    Create navbar

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,28 +1,16 @@
 import React, { ReactNode } from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 
-import { Button, CssBaseline } from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
+import NavBar from './navbar/NavBar';
 
 interface Props {
   children?: ReactNode;
   title?: string;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    button: {
-      backgroundColor: 'blue',
-      [theme.breakpoints.down('lg')]: {
-        backgroundColor: 'red',
-      },
-    },
-  })
-);
-
 const Layout = ({ children, title = 'RC Store' }: Props) => {
-  const classes = useStyles();
   return (
     <div>
       <CssBaseline />
@@ -31,19 +19,7 @@ const Layout = ({ children, title = 'RC Store' }: Props) => {
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
-      <header>
-        <nav>
-          <Link href='/'>
-            <a>Home</a>
-          </Link>{' '}
-          |{' '}
-          <Link href='/sign-in'>
-            <Button variant='contained' className={classes.button}>
-              Sign In
-            </Button>
-          </Link>{' '}
-        </nav>
-      </header>
+      <NavBar />
       {children}
       <footer>
         <hr />
