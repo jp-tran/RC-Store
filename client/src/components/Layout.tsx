5b9c92f20bfc9e604ad01f455d463d4cda33d348
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-02-01 16:09:10 -0400

    (refactor) Move ThemeProvider to Layout component
    So that the Layout can be applied to all components and have the custom
    theme already applied.

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,8 +1,10 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
 
+import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 
+import customTheme from '../config/theme';
 import NavBar from './navbar/NavBar';
 
 interface Props {
@@ -14,17 +16,22 @@ const Layout = ({ children, title = 'RC Store' }: Props) => {
   return (
     <div>
       <CssBaseline />
-      <Head>
-        <title>{title}</title>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <NavBar />
-      {children}
-      <footer>
-        <hr />
-        <span>I'm the footer</span>
-      </footer>
+      <ThemeProvider theme={customTheme}>
+        <Head>
+          <title>{title}</title>
+          <meta charSet='utf-8' />
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
+        </Head>
+        <NavBar />
+        {children}
+        <footer>
+          <hr />
+          <span>I'm the footer</span>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 };
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,17 +1,12 @@
-import { ThemeProvider } from '@material-ui/core/styles';
-
 import Layout from '../components/Layout';
 import ProductCatalog from '../components/ProductCatalog';
 import productList from '../components/getProducts';
-import customTheme from '../config/theme';
 
 const IndexPage = () => (
-  <ThemeProvider theme={customTheme}>
-    <Layout title='RC Store'>
-      <h1>Home</h1>
-      <ProductCatalog productList={productList} />
-    </Layout>
-  </ThemeProvider>
+  <Layout title='RC Store'>
+    <h1>Home</h1>
+    <ProductCatalog productList={productList} />
+  </Layout>
 );
 
 export default IndexPage;
