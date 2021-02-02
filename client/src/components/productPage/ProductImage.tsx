7a3865f73ad50273722591c9275c7ab162fb25c9
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-02-01 20:00:38 -0400

    (feat) Make Product Template components occupy 50% width
    Setting up layout.

--- client/src/components/productPage/ProductImage.tsx
+++ client/src/components/productPage/ProductImage.tsx
@@ -1,7 +1,18 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  child: {
+    width: '50%',
+    height: '50%',
+  },
+});
+
 export interface ProductImageProps {}
 
 const ProductImage: React.FunctionComponent<ProductImageProps> = () => {
-  return <div>Image comes here!</div>;
+  const classes = useStyles();
+
+  return <div className={classes.child}>Image comes here!</div>;
 };
 
 export default ProductImage;
--- client/src/components/productPage/ProductTemplate.tsx
+++ client/src/components/productPage/ProductTemplate.tsx
@@ -1,11 +1,22 @@
+import { makeStyles } from '@material-ui/core';
+
 import ProductImage from './ProductImage';
 import ProductInfo from './ProductInfo';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    width: '100%',
+  },
+});
 
 export interface ProductTemplateProps {}
 
 const ProductTemplate: React.FunctionComponent<ProductTemplateProps> = () => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.container}>
       <ProductImage />
       <ProductInfo />
     </div>
