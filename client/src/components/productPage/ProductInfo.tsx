a6238d65fcb0e4d9bc34e696bce03b51db374441
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-02-02 13:18:34 -0400

    (feat) Add product info layout and content on product page

--- client/src/components/productPage/ProductInfo.tsx
+++ client/src/components/productPage/ProductInfo.tsx
@@ -1,9 +1,19 @@
+import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   child: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: '20px',
     width: '50%',
+    whiteSpace: 'pre-wrap', // Needed to render line breaks
     background: 'white',
+  },
+  button: {
+    alignSelf: 'center',
   },
 });
 
@@ -11,8 +21,17 @@ export interface ProductInfoProps {}
 
 const ProductInfo: React.FunctionComponent<ProductInfoProps> = () => {
   const classes = useStyles();
-
-  return <div className={classes.child}>Information comes here!</div>;
+  // Need to encode product description: newlines, styling, etc.
+  const sometext: string =
+    'Product description\n\nA product with lots of nice feats.';
+  return (
+    <Container className={classes.child}>
+      <div>{sometext}</div>
+      <div className={classes.button}>
+        <Button color='primary'>Add to cart</Button>
+      </div>
+    </Container>
+  );
 };
 
 export default ProductInfo;
