ec190f5198705fb522db089886276322c93744b5
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-02-02 12:40:03 -0400

    (feat) Add layout to ProductImage component
    Makes the part of the product page that shows the product image have
    the desired layout. It still **doesn work** well for mobile display
    (needs rearranging flex to wrap for small screen sizes).

--- client/src/components/productPage/ProductImage.tsx
+++ client/src/components/productPage/ProductImage.tsx
@@ -3,7 +3,32 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles({
   child: {
     width: '50%',
-    height: '50%',
+    background: '#5f3dc4',
+    position: 'relative',
+  },
+
+  textContainer: {
+    marginTop: '20px',
+  },
+
+  text: {
+    margin: '50px 0 0 50px',
+    padding: '5px 20px',
+    display: 'inline',
+    background: '#7048e8',
+    color: 'white',
+  },
+
+  imageContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+
+  image: {
+    position: 'absolute',
+    top: '0',
+    maxHeight: '100%',
+    maxWidth: '100%',
   },
 });
 
@@ -12,7 +37,20 @@ export interface ProductImageProps {}
 const ProductImage: React.FunctionComponent<ProductImageProps> = () => {
   const classes = useStyles();
 
-  return <div className={classes.child}>Image comes here!</div>;
+  return (
+    <div className={classes.child}>
+      <div className={classes.textContainer}>
+        <h2 className={classes.text}>
+          Product Name
+          <br />
+        </h2>
+        <h3 className={classes.text}>$49.99 USD</h3>
+      </div>
+      <div className={classes.imageContainer}>
+        <img className={classes.image} src='/product_img.webp' />
+      </div>
+    </div>
+  );
 };
 
 export default ProductImage;
--- client/src/components/productPage/ProductInfo.tsx
+++ client/src/components/productPage/ProductInfo.tsx
@@ -1,7 +1,18 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  child: {
+    width: '50%',
+    background: 'white',
+  },
+});
+
 export interface ProductInfoProps {}
 
 const ProductInfo: React.FunctionComponent<ProductInfoProps> = () => {
-  return <div>Information comes here!</div>;
+  const classes = useStyles();
+
+  return <div className={classes.child}>Information comes here!</div>;
 };
 
 export default ProductInfo;
