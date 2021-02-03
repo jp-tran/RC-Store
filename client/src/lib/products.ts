618864251c204a11ff809e37f91ef7688a5e8783
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-02-03 17:33:52 -0400

    (feat) Add product routes (product page static content)
    Types for added functions have not been added yet.

--- client/src/lib/products.ts
+++ client/src/lib/products.ts
@@ -1,5 +1,9 @@
 import { ProductProps } from '../components/productCatalog/Product';
 
+export interface ProductPath {
+  params: { id: string };
+}
+
 const getProducts = (): ProductProps[] => [
   {
     productId: '0',
@@ -20,5 +24,21 @@ const getProducts = (): ProductProps[] => [
     description: 'A product of great quality.',
   },
 ];
+
+export const getAllProductPaths = () => {
+  const products: ProductProps[] = getProducts();
+  const productPaths: ProductPath[] = products.map((product) => ({
+    params: {
+      id: product.productId,
+    },
+  }));
+
+  return productPaths;
+};
+
+export const getProductData = (id: string): ProductProps => {
+  const products = getProducts();
+  return products.filter((product) => product.productId == id)[0];
+};
 
 export default getProducts;
--- client/src/pages/products/[id].tsx
+++ client/src/pages/products/[id].tsx
@@ -1,14 +1,53 @@
-import { useRouter } from 'next/router';
+import { ProductProps } from '../../components/productCatalog/Product';
 
 import Layout from '../../components/Layout';
+import {
+  getAllProductPaths,
+  ProductPath,
+  getProductData,
+} from '../../lib/products';
 
 export interface ProductPageProps {}
 
-const ProductPage: React.FunctionComponent<ProductPageProps> = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return <Layout>Post: {id}</Layout>;
+const ProductPage: React.FunctionComponent = ({ productProps }) => {
+  return <Layout>Hi!</Layout>;
 };
+
+export async function getStaticPaths(): Promise<{
+  paths: ProductPath[];
+  fallback: boolean;
+}> {
+  const paths: ProductPath[] = getAllProductPaths();
+  // Returns an array that looks like this:
+  // paths =
+  // [
+  //   {
+  //     params: {
+  //       id: '1'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: '2'
+  //     }
+  //   }
+  // ]
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+/* export async function getStaticProps(
+  params: ProductPath['params']
+): Promise<{ props: { productProps: ProductProps } }> */
+export async function getStaticProps({ params }) {
+  const productProps = getProductData(params.id);
+  return {
+    props: {
+      productProps,
+    },
+  };
+}
 
 export default ProductPage;
