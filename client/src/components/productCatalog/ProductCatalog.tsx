a36c1d6ac1b2b9575072725197e10f923d5897db
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-02-08 19:00:59 -0400

    (feat) Add grid structure

--- client/src/components/productCatalog/ProductCatalog.tsx
+++ client/src/components/productCatalog/ProductCatalog.tsx
@@ -3,9 +3,28 @@ import * as React from 'react';
 import { makeStyles } from '@material-ui/core';
 
 import Product, { ProductProps } from './Product';
+import ProductImage from '../productPage/ProductImage';
 
 const useStyles = makeStyles({
+  grid: {
+    display: 'grid',
+    gridTemplateAreas: `
+    "product1 product2"
+    "product1 product3"
+    "catalog catalog"
+    `,
+  },
+  product1: {
+    gridArea: 'product1',
+  },
+  product2: {
+    gridArea: 'product2',
+  },
+  product3: {
+    gridArea: 'product3',
+  },
   container: {
+    gridArea: 'catalog',
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'center',
@@ -22,20 +41,48 @@ const ProductCatalog: React.FunctionComponent<ProductCatalogProps> = (
   const { productList } = props;
   const classes = useStyles();
 
-  return (
-    <div className={classes.container}>
-      {productList.map((productProps) => {
-        const { productId, productName, imageSrc, description } = productProps;
+  interface FeaturedProduct {
+    productProps: ProductProps;
+    style: string;
+  }
 
+  // Get top 3 products and corresponding style
+  const featuredProducts: FeaturedProduct[] = [
+    { productProps: productList[0], style: classes.product1 },
+    { productProps: productList[1], style: classes.product2 },
+    { productProps: productList[2], style: classes.product3 },
+  ];
+  return (
+    <div className={classes.grid}>
+      {featuredProducts.map(({ productProps, style }) => {
         return (
-          <Product
-            productId={productId}
-            productName={productName}
-            imageSrc={imageSrc}
-            description={description}
-          />
+          <div className={style}>
+            <ProductImage
+              productName={productProps.productName}
+              imageSrc={productProps.imageSrc}
+            />
+          </div>
         );
       })}
+      <div className={classes.container}>
+        {productList.map((productProps) => {
+          const {
+            productId,
+            productName,
+            imageSrc,
+            description,
+          } = productProps;
+
+          return (
+            <Product
+              productId={productId}
+              productName={productName}
+              imageSrc={imageSrc}
+              description={description}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
