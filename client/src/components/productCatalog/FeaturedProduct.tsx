4155d1fc25b7a816b920d1aabe44b0641f1f6e5c
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-02-08 19:36:03 -0400

    (feat) Show product name and price on Featured Products

--- client/src/components/productCatalog/FeaturedProduct.tsx
+++ client/src/components/productCatalog/FeaturedProduct.tsx
@@ -3,9 +3,22 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles({
   container: {
     width: '100%',
+    position: 'relative',
   },
   image: {
     maxWidth: '100%',
+  },
+  textContainer: {
+    position: 'absolute',
+    top: '0',
+  },
+
+  text: {
+    margin: '0',
+    padding: '5px 20px',
+    display: 'inline',
+    background: 'black',
+    color: 'white',
   },
 });
 
@@ -22,6 +35,13 @@ const FeaturedProduct: React.FunctionComponent<FeaturedProductProps> = ({
   return (
     <div className={classes.container}>
       <img src={imageSrc} className={classes.image} />
+      <div className={classes.textContainer}>
+        <h2 className={classes.text}>
+          {productName}
+          <br />
+        </h2>
+        <h3 className={classes.text}>$49.99 USD</h3>
+      </div>
     </div>
   );
 };
