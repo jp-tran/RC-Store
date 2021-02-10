9c74b151d9def6563a661ec1e18023e6f34f3c2d
Phuoc Tran <phuoctran27@gmail.com>
2021-02-10 12:55:00 -0800

    Clean up code before asking question

--- client/src/pages/use-shopping-cart.tsx
+++ client/src/pages/use-shopping-cart.tsx
@@ -11,21 +11,8 @@ const CheckoutPage: NextPage = () => {
   return (
     <Layout title='Shopping Cart'>
       <h1>Shopping Cart</h1>
-      <CartProvider
-        mode='client-only'
-        stripe={getStripe()}
-        // The URL to which Stripe should send customers when payment is complete.
-        successUrl='http://localhost:3000/result?session_id={CHECKOUT_SESSION_ID}'
-        // The URL to which Stripe should send customers when payment is canceled.
-        cancelUrl='http://localhost:3000/use-shopping-cart'
-        currency='USD'
-        // https://stripe.com/docs/payments/checkout/client#collect-shipping-address
-        allowedCountries={['US', 'GB', 'CA']}
-        // https://stripe.com/docs/payments/checkout/client#collect-billing-address
-        billingAddressCollection={true}
-      >
+      <CartProvider mode='checkout-session' stripe={getStripe()} currency='usd'>
         <CartSummary />
-        {/* <p>Stuff</p> */}
       </CartProvider>
     </Layout>
   );
