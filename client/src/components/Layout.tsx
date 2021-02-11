19c8f4f6294e01de34e85d1a3250e9f379356161
Phuoc Tran <phuoctran27@gmail.com>
2021-02-11 00:24:18 -0800

    Add Cart context to all children of Layout component

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -8,8 +8,8 @@ import { CssBaseline } from '@material-ui/core';
 
 import customTheme from '../config/theme';
 import NavBar from './navbar/NavBar';
-
 import Footer from './footer/Footer';
+import CartProvider from './shoppingCart/CartProvider';
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
@@ -25,21 +25,23 @@ const Layout = ({ children, title = 'RC Store' }: Props) => {
     <div>
       <CssBaseline />
       <Elements stripe={stripePromise}>
-        <ThemeProvider theme={customTheme}>
-          <Head>
-            <title>{title}</title>
-            <meta charSet='utf-8' />
-            <meta
-              name='viewport'
-              content='initial-scale=1.0, width=device-width'
-            />
-          </Head>
-          <NavBar />
-          {children}
-          <footer>
-            <Footer />
-          </footer>
-        </ThemeProvider>
+        <CartProvider>
+          <ThemeProvider theme={customTheme}>
+            <Head>
+              <title>{title}</title>
+              <meta charSet='utf-8' />
+              <meta
+                name='viewport'
+                content='initial-scale=1.0, width=device-width'
+              />
+            </Head>
+            <NavBar />
+            {children}
+            <footer>
+              <Footer />
+            </footer>
+          </ThemeProvider>
+        </CartProvider>
       </Elements>
     </div>
   );
