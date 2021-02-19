a34aa1f19fee836b4d60a724af62f311db67b0ad
Phuoc Tran <phuoctran27@gmail.com>
2021-02-18 23:03:47 -0800

    Set up basic GraphQL endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
 import 'reflect-metadata';
 import 'dotenv-safe/config';
+import express from 'express';
 import { createConnection } from 'typeorm';
 import { Product } from './entities/Product';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
-  const conn = await createConnection({
+  await createConnection({
     type: 'postgres',
     host: 'localhost',
-    port: parseInt(process.env.PORT as string),
+    port: 5432,
     database: 'recurse-store',
     username: process.env.DATATBASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
@@ -15,6 +19,22 @@ const main = async () => {
     synchronize: true, //entities will be synced with database every time the app is ran
     entities: [Product],
     uuidExtension: 'pgcrypto',
+  });
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  // create graphql endpoint on express
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(parseInt(process.env.PORT as string), () => {
+    console.log(`Server started on port: ${process.env.PORT}`);
   });
 };
 
