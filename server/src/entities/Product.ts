2614c8358755ba3c932a619ed44b67c31980fce7
Phuoc Tran <phuoctran27@gmail.com>
2021-02-18 23:05:00 -0800

    Expose all columns of product table to GraphQL schema

--- server/src/entities/Product.ts
+++ server/src/entities/Product.ts
@@ -1,38 +1,53 @@
 import { ObjectType, Field } from 'type-graphql';
-import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  CreateDateColumn,
+} from 'typeorm';
 
 @ObjectType()
 @Entity()
 export class Product extends BaseEntity {
-  @Field()
+  @Field() // exposes this column to the graphql schema
   @PrimaryGeneratedColumn('uuid')
   sku!: string;
 
+  @Field()
   @Column({ length: 100 })
   name!: string;
 
+  @Field()
   @Column()
   description: string;
 
+  @Field()
   @Column()
   longDescription: string;
 
+  @Field()
   @Column()
   price!: number;
 
-  @Column()
+  @Field()
+  @Column({ default: 'USD' })
   currency!: string;
 
+  @Field()
   @Column()
   remainingQuantity: number;
 
+  @Field()
   @Column()
   image: string;
 
+  @Field()
   @Column()
   seller: number; // need to check this
 
-  @Column()
+  @Field()
+  @CreateDateColumn()
   datePosted: Date;
 
   // column for categories that item belongs to?
