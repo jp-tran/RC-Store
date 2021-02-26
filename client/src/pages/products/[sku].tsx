a267c142fd4f05f0d8ccfb7b790eae1789c497fc
Phuoc Tran <phuoctran27@gmail.com>
2021-02-25 21:22:31 -0800

    Fetch individual product using GraphQL

---
+++ client/src/pages/products/[sku].tsx
@@ -0,0 +1,50 @@
+import { gql } from '@apollo/client';
+import Layout from '../../components/Layout';
+import ProductTemplate from '../../components/productPage/ProductTemplate';
+import { initializeApollo } from '../../lib/apolloClient';
+import { ProductProps } from '../../types';
+
+const GET_PRODUCT = gql`
+  query Product($sku: String!) {
+    product(sku: $sku) {
+      sku
+      name
+      price
+      currency
+      image
+      description
+      longDescription
+    }
+  }
+`;
+
+const ProductPage = ({ product }: { product: ProductProps }) => {
+  return (
+    <Layout>
+      <ProductTemplate product={product} />
+    </Layout>
+  );
+};
+
+interface ProductPath {
+  params: { sku: string };
+}
+
+export async function getServerSideProps({ params }: ProductPath) {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query({
+    query: GET_PRODUCT,
+    variables: { sku: params.sku },
+  });
+
+  const product = data.product;
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export default ProductPage;
