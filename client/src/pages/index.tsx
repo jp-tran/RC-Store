0229fb0bfca7d90aea00ad1dad64f0092948007f
Phuoc Tran <phuoctran27@gmail.com>
2021-02-25 21:18:02 -0800

    Fetch product data using GraphQL

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,18 +1,42 @@
+import { gql } from '@apollo/client';
 import Layout from '../components/Layout';
 import ProductCatalog from '../components/productCatalog/ProductCatalog';
-import products from '../lib/products.json';
+import { initializeApollo } from '../lib/apolloClient';
 import { ProductProps } from '../types';
 
-const IndexPage: React.FunctionComponent<{ products: ProductProps[] }> = ({
-  products,
-}) => (
-  <Layout title='RC Store'>
-    <h1>Home</h1>
-    <ProductCatalog productList={products} />
-  </Layout>
-);
+const GET_ALL_PRODUCTS = gql`
+  query Products($isRecurseCenterMerch: Boolean) {
+    products(isRecurseCenterMerch: $isRecurseCenterMerch) {
+      sku
+      name
+      price
+      currency
+      image
+      description
+      longDescription
+    }
+  }
+`;
 
-export async function getStaticProps() {
+const IndexPage = ({ products }: { products: ProductProps[] }) => {
+  return (
+    <Layout title='RC Store'>
+      <h1>Home</h1>
+      <ProductCatalog productList={products} />
+    </Layout>
+  );
+};
+
+export async function getServerSideProps() {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query({
+    query: GET_ALL_PRODUCTS,
+    variables: { isRecurseCenterMerch: true },
+  });
+
+  const products = data.products;
+
   return {
     props: {
       products,
