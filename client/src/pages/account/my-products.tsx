ec12a2a446b2b77c27d16b86af2638364a4f5c14
Phuoc Tran <phuoctran27@gmail.com>
2021-02-27 17:58:16 -0800

    Secure page and fetch test products

--- client/src/pages/account/my-products.tsx
+++ client/src/pages/account/my-products.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import { useSession } from 'next-auth/client';
+import { useQuery } from '@apollo/client';
 import MyProducts from '../../components/account/MyProducts';
 import Layout from '../../components/Layout';
+import GET_MY_PRODUCTS from '../../graphql/queries/getMyProducts';
+import { ProductProps } from '../../types';
 
 const MyProductsPage = () => {
-  return (
-    <Layout title='Account | My Products'>
-      <MyProducts />
-    </Layout>
-  );
+  const [session, loading] = useSession();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!loading && !session) {
+    return <p>Access Denied</p>;
+  }
+
+  if (session) {
+    const { loading, data } = useQuery(GET_MY_PRODUCTS, {
+      variables: { sellerID: 1 },
+    });
+
+    if (loading) {
+      return <p>Loading...</p>;
+    }
+
+    const products: ProductProps[] = data.products;
+
+    return (
+      <Layout title='Account | My Products'>
+        <MyProducts products={products} />
+      </Layout>
+    );
+  }
 };
 
 export default MyProductsPage;
