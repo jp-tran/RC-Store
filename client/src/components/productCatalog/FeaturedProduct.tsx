fd1f0edecfac686b2ae6133a47ca2553dc729ed5
Phuoc Tran <phuoctran27@gmail.com>
2021-03-03 21:41:34 -0800

    Redesign product layout on home page

--- client/src/components/productCatalog/FeaturedProduct.tsx
+++ client/src/components/productCatalog/FeaturedProduct.tsx
@@ -1,34 +1,48 @@
 import Link from 'next/link';
 
-import { makeStyles } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
 
 import { ProductProps } from '../../types';
+import { formatCurrencyString } from 'use-shopping-cart';
 
-const useStyles = makeStyles({
-  container: {
-    width: '100%',
-    position: 'relative',
-    overflow: 'hidden',
-  },
-  image: {
-    maxWidth: '100%',
-    transition: 'transform(.2s)',
-    '&:hover': {
-      transform: 'scale(1.1)',
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      width: '100%',
+      position: 'relative',
+      overflow: 'hidden',
+      display: 'flex',
+      alignItems: 'flex-end',
+      background: 'white',
+      boxShadow: '0 1px 5px 0px rgba(38,59,94,0.3)',
+      border: '2px solid white',
+      '&:hover,&:focus-within': {
+        borderColor: theme.palette.primary.main,
+        boxShadow: '0 2px 15px 5px rgba(38,59,94,0.3)',
+        transition: '0.25s ease-out',
+      },
     },
-  },
-  textContainer: {
-    position: 'absolute',
-    top: '0',
-  },
+    link: {
+      '&:focus': {
+        outline: 'none',
+      },
+    },
+    image: {
+      maxWidth: '100%',
+    },
+    textContainer: {
+      position: 'absolute',
+      top: '0',
+    },
 
-  text: {
-    margin: '0',
-    padding: '5px 20px',
-    background: 'black',
-    color: 'white',
-  },
-});
+    text: {
+      margin: '0',
+      padding: '5px 20px',
+      background: 'black',
+      color: 'white',
+    },
+  })
+);
 
 export interface FeaturedProductProps {
   name: string;
@@ -37,16 +51,22 @@ export interface FeaturedProductProps {
   image?: string;
 }
 
-const FeaturedProduct = ({ product }: { product: ProductProps }) => {
+const FeaturedProduct = ({
+  product,
+  productStyle,
+}: {
+  product: ProductProps;
+  productStyle: string;
+}) => {
   const classes = useStyles();
 
-  const { name, sku, image, price } = product;
+  const { name, sku, image, price, currency } = product;
 
   return (
-    <div className={classes.container}>
+    <div className={`${classes.container} ${productStyle}`}>
       <Link href={`/products/${sku}`}>
-        <a>
-          <img src={image} className={classes.image} />
+        <a className={classes.link}>
+          <img src={image} className={classes.image} alt={name} />
         </a>
       </Link>
 
@@ -55,7 +75,9 @@ const FeaturedProduct = ({ product }: { product: ProductProps }) => {
           {name}
           <br />
         </h2>
-        <h3 className={classes.text}>${price.toString()}</h3>
+        <h3 className={classes.text}>
+          {formatCurrencyString({ value: price, currency })}
+        </h3>
       </div>
     </div>
   );
--- client/src/components/productCatalog/ProductCatalog.tsx
+++ client/src/components/productCatalog/ProductCatalog.tsx
@@ -1,64 +1,115 @@
-import { makeStyles } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
 
-import ProductCard from './ProductCard';
 import FeaturedProduct from './FeaturedProduct';
 import { ProductProps } from '../../types';
 
-const useStyles = makeStyles({
-  grid: {
-    display: 'grid',
-    gridTemplateColumns: '4fr 2fr',
-    gridTemplateAreas: `
-    "product1 product2"
-    "product1 product3"
-    "catalog catalog"
-    `,
-    placeItems: 'center center',
-    background: 'white',
-  },
-  product1: {
-    gridArea: 'product1',
-  },
-  product2: {
-    gridArea: 'product2',
-  },
-  product3: {
-    gridArea: 'product3',
-  },
-  container: {
-    gridArea: 'catalog',
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      maxWidth: theme.breakpoints.values.xl,
+      padding: '20px',
+    },
+    grid: {
+      display: 'grid',
+      justifyContent: 'center',
+      gridGap: '20px',
+      [theme.breakpoints.up('xs')]: {
+        gridTemplateColumns: 'minmax(200px, 400px)',
+        gridAutoRows: 'minmax(300px, 1fr)',
+        gridTemplateAreas: `
+        "product1"
+        "product2"
+        "product3"
+        `,
+      },
+      [theme.breakpoints.up('md')]: {
+        gridTemplateColumns: 'minmax(300px, 4fr) minmax(300px, 2fr)',
+        gridTemplateRows: 'minmax(300px, 1fr) minmax(300px, 1fr)',
+        gridAutoRows: '400px',
+        gridTemplateAreas: `
+        "product1 product2"
+        "product1 product3"
+        `,
+      },
+    },
+    product1: {
+      gridArea: 'product1',
+    },
+    product2: {
+      gridArea: 'product2',
+    },
+    product3: {
+      gridArea: 'product3',
+    },
+    nonFeaturedProducts: {
+      marginTop: '20px',
+      display: 'grid',
+      gridGap: '20px',
+      gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+      gridAutoRows: '400px',
+      justifyContent: 'center',
+    },
+  })
+);
 
 const ProductCatalog = ({ productList }: { productList: ProductProps[] }) => {
   const classes = useStyles();
 
-  interface FeaturedProductAndStyle {
-    productProps: ProductProps;
-    style: string;
+  // Get 3 merch items and corresponding style for featured panel
+  const featuredProductsStyle = [
+    classes.product1,
+    classes.product2,
+    classes.product3,
+  ];
+
+  const featuredSKUs = [
+    'e8a96fa1-cba0-4cd6-8a6a-ba69e5fca0b9',
+    '796cd7b2-30ac-4ec4-a66e-3735bdbc029c',
+    'e0f1ed68-933f-477c-b5d3-7394a242e6ca',
+  ];
+
+  const featuredProducts: ProductProps[] = [
+    productList[0],
+    productList[1],
+    productList[2],
+  ];
+
+  const nonFeaturedProducts: ProductProps[] = [];
+
+  for (const prod of productList) {
+    if (prod.sku === featuredSKUs[0]) {
+      featuredProducts[0] = prod;
+    } else if (prod.sku == featuredSKUs[1]) {
+      featuredProducts[1] = prod;
+    } else if (prod.sku == featuredSKUs[2]) {
+      featuredProducts[2] = prod;
+    } else {
+      nonFeaturedProducts.push(prod);
+    }
   }
 
-  // Get top 3 products and corresponding style
-  const featuredProducts: FeaturedProductAndStyle[] = [
-    { productProps: productList[0], style: classes.product1 },
-    { productProps: productList[1], style: classes.product2 },
-    { productProps: productList[2], style: classes.product3 },
-  ];
   return (
-    <div className={classes.grid}>
-      {featuredProducts.map(({ productProps, style }) => {
-        return (
-          <div className={style} key={productProps.sku}>
-            <FeaturedProduct product={productProps} />
-          </div>
-        );
-      })}
-      <div className={classes.container}>
-        {productList.map((productProps) => {
-          return <ProductCard product={productProps} key={productProps.sku} />;
+    <div className={classes.container} id='RC-merch'>
+      <div className={classes.grid}>
+        {featuredProducts.map((productProps, idx) => {
+          return (
+            <FeaturedProduct
+              product={productProps}
+              productStyle={featuredProductsStyle[idx]}
+              key={productProps.sku}
+            />
+          );
+        })}
+      </div>
+      <div className={classes.nonFeaturedProducts}>
+        {nonFeaturedProducts.map((productProps) => {
+          return (
+            <FeaturedProduct
+              product={productProps}
+              productStyle=''
+              key={productProps.sku}
+            />
+          );
         })}
       </div>
     </div>
