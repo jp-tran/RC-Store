c88e676e09fbea651f02272046f6496be064b0eb
Phuoc Tran <phuoctran27@gmail.com>
2021-03-03 23:58:44 -0800

    Implement minor styling changes

--- client/src/components/shoppingCart/CartSummary.tsx
+++ client/src/components/shoppingCart/CartSummary.tsx
@@ -3,9 +3,11 @@ import { useShoppingCart } from 'use-shopping-cart';
 import {
   Button,
   Container,
+  createStyles,
   Divider,
   IconButton,
   makeStyles,
+  Theme,
   Typography,
 } from '@material-ui/core';
 import IncrementIcon from '@material-ui/icons/Add';
@@ -13,55 +15,76 @@ import DecrementIcon from '@material-ui/icons/Remove';
 
 import CheckoutButton from './CheckoutButton';
 
-const useStyles = makeStyles({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  subContainer: {
-    display: 'flex',
-    width: '100%',
-    maxWidth: '768px',
-  },
-  itemSummary: {
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-  },
-  cartSummary: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-  },
-  leftBlock: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-  },
-  quantity: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: '-6px',
-  },
-  itemActions: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  iconButton: {
-    borderRadius: '50%',
-    border: '1px solid #3c89c3',
-    height: '24px',
-    width: '24px',
-    margin: '5px',
-  },
-  removeButton: {
-    height: '24px',
-    margin: '0.5rem 1rem 0.5rem 1rem',
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    pageTitle: {
+      width: '100%',
+      [theme.breakpoints.up('xs')]: {
+        fontSize: '1.5rem',
+      },
+      [theme.breakpoints.up('sm')]: {
+        fontSize: '2.5rem',
+      },
+      [theme.breakpoints.up('md')]: {
+        fontSize: '3.5rem',
+      },
+    },
+    checkoutDetails: {
+      background: 'white',
+      width: '100%',
+      maxWidth: '1024px',
+      padding: '1rem',
+      marginBottom: '1rem',
+      boxShadow: '0 1px 5px 0px rgba(38,59,94,0.3)',
+    },
+    subContainer: {
+      display: 'flex',
+      width: '100%',
+    },
+    itemSummary: {
+      justifyContent: 'space-between',
+      alignItems: 'flex-start',
+    },
+    cartSummary: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+    },
+    leftBlock: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+    },
+    quantity: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: '-6px',
+    },
+    itemActions: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    iconButton: {
+      borderRadius: '50%',
+      border: '1px solid #3c89c3',
+      height: '24px',
+      width: '24px',
+      margin: '5px',
+    },
+    removeButton: {
+      height: '24px',
+      margin: '0.5rem 1rem 0.5rem 1rem',
+    },
+  })
+);
 
 const CartSummary = () => {
   const classes = useStyles();
@@ -82,10 +105,7 @@ const CartSummary = () => {
 
     // all of your basic product data still exists (i.e. name, image, price)
     cart.push(
-      <div
-        style={{ width: '100%', maxWidth: '768px', marginBottom: '1rem' }}
-        key={cartEntry.sku}
-      >
+      <div style={{ width: '100%', marginBottom: '1rem' }} key={cartEntry.sku}>
         <div className={`${classes.subContainer} ${classes.itemSummary}`}>
           <div className={classes.leftBlock}>
             <Typography>{cartEntry.name}</Typography>
@@ -131,19 +151,21 @@ const CartSummary = () => {
   return (
     <Container className={classes.container}>
       <Typography
-        className={classes.subContainer}
+        className={classes.pageTitle}
         component='h1'
         variant='h3'
         style={{ margin: '2rem 0' }}
       >
-        Shopping Cart
+        Shopping Cart &#128722;
       </Typography>
-      {cart}
-      <div className={`${classes.subContainer} ${classes.cartSummary}`}>
-        <Typography>{`Total: ${formattedTotalPrice}`}</Typography>
-        <div>
-          <Button onClick={() => clearCart()}>Clear Cart</Button>
-          <CheckoutButton />
+      <div className={classes.checkoutDetails}>
+        {cart}
+        <div className={`${classes.subContainer} ${classes.cartSummary}`}>
+          <Typography>{`Total: ${formattedTotalPrice}`}</Typography>
+          <div>
+            <Button onClick={() => clearCart()}>Clear Cart</Button>
+            <CheckoutButton />
+          </div>
         </div>
       </div>
     </Container>
