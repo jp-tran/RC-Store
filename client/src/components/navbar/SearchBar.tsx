53b32803d30f80dd419769a194533f416ff72f8c
Phuoc Tran <phuoctran27@gmail.com>
2021-03-06 00:11:01 -0800

    Make search bar a controlled component with new styling

--- client/src/components/navbar/SearchBar.tsx
+++ client/src/components/navbar/SearchBar.tsx
@@ -1,67 +1,89 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useRouter } from 'next/router';
 
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { InputBase } from '@material-ui/core';
+import { Button, InputBase } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+
+import { SearchBarContext } from '../../contexts/SearchBarContext';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     search: {
       position: 'relative',
-      borderRadius: theme.shape.borderRadius,
       backgroundColor: 'white',
       transition: '0.2s linear',
       border: '2px solid #949494',
       '&:focus-within': {
         borderColor: 'green',
       },
-      // '&:hover': {
-      //   backgroundColor: 'rgba(153,153,153,0.1)',
-      // },
       width: '100%',
       height: '2.5rem',
       display: 'flex',
       alignItems: 'center',
     },
-    searchIcon: {
-      color: 'black',
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
     input: {
       color: 'black',
       padding: theme.spacing(1, 1, 1, 0),
-      marginLeft: `calc(1em + ${theme.spacing(4)}px)`, //margin to account for search icon
+      marginLeft: '0.5rem',
       width: '100%',
       height: '100%',
+    },
+    searchButton: {
+      borderRadius: 0,
     },
   })
 );
 
 const SearchBar = () => {
   const classes = useStyles();
+
+  const router = useRouter();
+
+  const { query, setQuery } = useContext(SearchBarContext);
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setQuery(event.target.value as string);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    router.push(
+      {
+        pathname: '/search',
+        query: { query },
+      },
+      undefined,
+      { shallow: true } // change path without reloading page
+    );
+  };
+
   return (
-    <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <SearchIcon />
-      </div>
+    <form className={classes.search} onSubmit={handleSubmit} id='search_form'>
       <InputBase
         className={classes.input}
         placeholder='Search...'
         type='search'
-        // value={searchQuery}
-        // onChange={handleChange}
+        name='query'
+        value={query}
+        onChange={handleChange}
         inputProps={{
           spellCheck: 'false',
-          maxLength: '100',
+          maxLength: '50',
         }}
       />
-    </div>
+      <Button
+        type='submit'
+        form='search_form'
+        value='Submit'
+        variant='contained'
+        color='primary'
+        disableElevation={true}
+        className={classes.searchButton}
+      >
+        <SearchIcon />
+      </Button>
+    </form>
   );
 };
 
