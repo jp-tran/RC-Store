417fab77969d2bebe5a12ff38ec2f6ec68ac53ef
Phuoc Tran <phuoctran27@gmail.com>
2021-03-08 12:13:58 -0800

    Change gradient of shopping cart page

--- client/src/pages/shoppingcart/index.tsx
+++ client/src/pages/shoppingcart/index.tsx
@@ -3,12 +3,13 @@ import { useShoppingCart } from 'use-shopping-cart';
 import Layout from '../../components/Layout';
 import CartSummaryNoSSR from '../../components/shoppingCart/CartSummaryNoSSR';
 import EmptyCartMessage from '../../components/shoppingCart/EmptyCartMessage';
+import gradients from '../../config/gradients';
 
 const ShoppingCartPage: NextPage = () => {
   const { cartCount } = useShoppingCart();
 
   return (
-    <Layout title='Shopping Cart'>
+    <Layout title='Shopping Cart' gradient={gradients.green}>
       {cartCount ? <CartSummaryNoSSR /> : <EmptyCartMessage />}
     </Layout>
   );
