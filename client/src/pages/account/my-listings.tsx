64971273627ec01a415ec4b9ebf7a55b6d25008c
Phuoc Tran <phuoctran27@gmail.com>
2021-03-09 14:14:06 -0800

    Get user ID programmatically and add error handling

--- client/src/pages/account/my-listings.tsx
+++ client/src/pages/account/my-listings.tsx
@@ -4,7 +4,7 @@ import { useQuery } from '@apollo/client';
 import MyProducts from '../../components/account/MyProducts';
 import Layout from '../../components/Layout';
 import GET_MY_LISTINGS from '../../graphql/queries/getMyListings';
-import { ProductProps } from '../../types';
+import { CustomUser, ProductProps } from '../../types';
 import gradients from '../../config/gradients';
 
 const MyProductsPage = () => {
@@ -19,13 +19,17 @@ const MyProductsPage = () => {
   }
 
   if (session) {
-    const { loading, data } = useQuery(GET_MY_LISTINGS, {
-      variables: { sellerID: 4279.0 },
+    const customUser: CustomUser = session.user;
+
+    const { loading, data, error } = useQuery(GET_MY_LISTINGS, {
+      variables: { sellerID: customUser.id },
     });
 
     if (loading) {
       return <p>Loading...</p>;
     }
+
+    if (error) return <p>Error fetching data</p>;
 
     const products: ProductProps[] = data.listings;
 
