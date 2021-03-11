aa21417d2dde53d88e021b4394d92fb098e39243
Phuoc Tran <phuoctran27@gmail.com>
2021-03-11 13:14:37 -0800

    Show notication after adding item to cart

--- client/src/components/shoppingCart/AddToCartButton.tsx
+++ client/src/components/shoppingCart/AddToCartButton.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useShoppingCart } from 'use-shopping-cart';
 
-import { Button, createStyles, makeStyles } from '@material-ui/core';
-import { ProductProps } from '../../types';
+import {
+  Button,
+  createStyles,
+  makeStyles,
+  Snackbar,
+  Theme,
+} from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
-const useStyles = makeStyles(() =>
+import { ProductProps } from '../../types';
+import NextLink from '../NextLink';
+
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     button: {
       margin: '10px',
+    },
+    snackbar: {
+      backgroundColor: theme.palette.primary.main,
+      color: 'white',
+    },
+    snackbarButton: {
+      color: theme.palette.secondary.dark,
     },
   })
 );
@@ -21,6 +38,7 @@ const AddToCartButton = ({
 }) => {
   const classes = useStyles();
   const { addItem, cartDetails, incrementItem } = useShoppingCart();
+  const [snackbarIsOpen, setSnackbarIsOpen] = useState(false);
 
   const handleClick = () => {
     // search for the variation that matches the selected size
@@ -37,18 +55,62 @@ const AddToCartButton = ({
     } else {
       addItem(selectedItem);
     }
+
+    setSnackbarIsOpen(true);
+  };
+
+  const handleClose = (_event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackbarIsOpen(false);
   };
 
   return (
-    <Button
-      className={classes.button}
-      onClick={handleClick}
-      color='primary'
-      size='large'
-      variant='contained'
-    >
-      Add to Cart
-    </Button>
+    <>
+      <Button
+        className={classes.button}
+        onClick={handleClick}
+        color='primary'
+        size='large'
+        variant='contained'
+      >
+        Add to Cart
+      </Button>
+      <Snackbar
+        ContentProps={{
+          classes: {
+            root: classes.snackbar,
+          },
+        }}
+        open={snackbarIsOpen}
+        autoHideDuration={5000}
+        onClose={handleClose}
+        message='Added to cart!'
+        action={
+          <>
+            <Button
+              className={classes.snackbarButton}
+              size='small'
+              onClick={handleClose}
+              component={NextLink}
+              href='/shoppingcart'
+            >
+              VIEW CART
+            </Button>
+            <IconButton
+              size='small'
+              aria-label='close'
+              color='inherit'
+              onClick={handleClose}
+            >
+              <CloseIcon fontSize='small' />
+            </IconButton>
+          </>
+        }
+      />
+    </>
   );
 };
 
