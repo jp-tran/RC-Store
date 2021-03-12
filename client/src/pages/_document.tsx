2ab16cf7ec73ed5e07b07b78c29a98048817d1fe
Phuoc Tran <phuoctran27@gmail.com>
2021-03-12 13:45:50 -0800

    Create favicon

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/styles';
-import theme from '../config/theme';
 
 class MyDocument extends Document {
   render() {
@@ -9,7 +8,27 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet='utf-8' />
-          <meta name='theme-color' content={theme.palette.primary.main} />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#3dc06c' />
+          <meta name='msapplication-TileColor' content='#00a300' />
+          <meta name='theme-color' content='#ffffff' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
             rel='stylesheet'
